4227fd3fa79943eef88a5f2725d4c10074376ca7
sanjaybabu123 <sanjaybabu700@gmail.com>
2022-04-29 13:21:57 +0530

    1st commit

---
+++ myproject/src/test/java/sample/FirstClassTest.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class FirstClassTest {
+@Test
+public void Hi()
+{
+	System.out.println("hi all");
+}
+}
---
+++ myproject/src/test/java/sample/ThirdClassTest.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class ThirdClassTest {
+	@Test(groups="SmokeTest")
+	public void Bye()
+	{
+		System.out.println("hi all");
+		System.out.println("Bye all");
+	}
+}
